5084d7e0a98a3d0c1f26bea4d4d1d12a65e55655
dawidogg <dawidogg.feed@gmail.com>
2023-03-16 19:02:15 +0300

    Add Part 3 and Part 5

--- basic_gates.v
+++ basic_gates.v
@@ -8,9 +8,9 @@ module AND_4(out, a);
     output out;
     input[3:0] a;
     wire and_0_1, and_2_3;
-    AND a1(and0_1, a[0], a[1]);
-    AND a2(and2_3, a[2], a[3]);
-    AND a3(out, and0_1, and2_3);
+    AND and0(and0_1, a[0], a[1]);
+    AND and1(and2_3, a[2], a[3]);
+    AND and2(out, and0_1, and2_3);
 endmodule
 
 module OR(a_or_b, a, b);
@@ -22,18 +22,18 @@ endmodule
 module OR_8(out, a);
     output out;
     input[7:0] a;
-    wire[3:0] stage1;
-    wire[1:0] stage2;
+    wire[3:0] stage0;
+    wire[1:0] stage1;
 
-    OR o0(stage1[0], a[0], a[1]);
-    OR o1(stage1[1], a[2], a[3]);
-    OR o2(stage1[2], a[4], a[5]);
-    OR o3(stage1[3], a[6], a[7]);
+    OR or0(stage0[0], a[0], a[1]);
+    OR or1(stage0[1], a[2], a[3]);
+    OR or2(stage0[2], a[4], a[5]);
+    OR or3(stage0[3], a[6], a[7]);
 
-    OR o4(stage2[0], stage1[0], stage1[1]);
-    OR o5(stage2[1], stage1[2], stage1[3]);
+    OR or4(stage1[0], stage0[0], stage0[1]);
+    OR or5(stage1[1], stage0[2], stage0[3]);
 
-    OR o6(out, stage2[0], stage2[1]);
+    OR or6(out, stage1[0], stage0[1]);
 endmodule
 
 module NOT(not_a, a);
@@ -45,11 +45,19 @@ endmodule
 module XOR(a_xor_b, a, b);
     output a_xor_b;
     input a, b;
-    assign a_xor_b = a ^ b;
+    assign a_xor_b = a&~b | ~a&b;
 endmodule
 
 module NAND(a_nand_b, a, b);
     output a_nand_b;
     input a, b;
-    assign a_nand_b = a ~& b;
+    assign a_nand_b = ~(a & b);
+endmodule
+
+module NAND_3(out, a);
+    output out;
+    input[2:0] a;
+    wire and_0_1;
+    AND and0(and_0_1, a[0], a[1]);
+    NAND nand0(out, and_0_1, a[2]);
 endmodule
--- mux_decoder.v
+++ mux_decoder.v
@@ -5,18 +5,20 @@ module MUX_8_1(out, s, d);
     wire[2:0] not_s;
     wire[7:0] minterm;
     
-    NOT n0(not_s[0], s[0]);
-    NOT n1(not_s[1], s[1]);
-    NOT n2(not_s[2], s[2]);
-    AND_4 a0(minterm[0], {not_s[2], not_s[1], not_s[0], d[0]});
-    AND_4 a1(minterm[1], {not_s[2], not_s[1], s[0], d[1]});
-    AND_4 a2(minterm[2], {not_s[2], s[1], not_s[0], d[2]});
-    AND_4 a3(minterm[3], {not_s[2], s[1], s[0], d[3]});
-    AND_4 a4(minterm[4], {s[2], not_s[1], not_s[0], d[4]});
-    AND_4 a5(minterm[5], {s[2], not_s[1], s[0], d[5]});
-    AND_4 a6(minterm[6], {s[2], s[1], not_s[0], d[6]});
-    AND_4 a7(minterm[7], {s[2], s[1], s[0], d[7]});
-    OR_8 o(out, minterm);
+    NOT not0(not_s[0], s[0]);
+    NOT not1(not_s[1], s[1]);
+    NOT not2(not_s[2], s[2]);
+    
+    AND_4 and0(minterm[0], {not_s[2], not_s[1], not_s[0], d[0]});
+    AND_4 and1(minterm[1], {not_s[2], not_s[1], s[0], d[1]});
+    AND_4 and2(minterm[2], {not_s[2], s[1], not_s[0], d[2]});
+    AND_4 and3(minterm[3], {not_s[2], s[1], s[0], d[3]});
+    AND_4 and4(minterm[4], {s[2], not_s[1], not_s[0], d[4]});
+    AND_4 and5(minterm[5], {s[2], not_s[1], s[0], d[5]});
+    AND_4 and6(minterm[6], {s[2], s[1], not_s[0], d[6]});
+    AND_4 and7(minterm[7], {s[2], s[1], s[0], d[7]});
+
+    OR_8 or0(out, minterm);
 endmodule
 
 module Decoder_3_8(out, s);
@@ -25,16 +27,16 @@ module Decoder_3_8(out, s);
     wire[2:0] not_s;
     parameter enable = 1'b1;
 
-    NOT n0(not_s[0], s[0]);
-    NOT n1(not_s[1], s[1]);
-    NOT n2(not_s[2], s[2]);
+    NOT not0(not_s[0], s[0]);
+    NOT not1(not_s[1], s[1]);
+    NOT not2(not_s[2], s[2]);
 
-    AND_4 a0(out[0], {not_s[2], not_s[1], not_s[0], enable});
-    AND_4 a1(out[1], {not_s[2], not_s[1], s[0], enable});
-    AND_4 a2(out[2], {not_s[2], s[1], not_s[0], enable});
-    AND_4 a3(out[3], {not_s[2], s[1], s[0], enable});
-    AND_4 a4(out[4], {s[2], not_s[1], not_s[0], enable});
-    AND_4 a5(out[5], {s[2], not_s[1], s[0], enable});
-    AND_4 a6(out[6], {s[2], s[1], not_s[0], enable});
-    AND_4 a7(out[7], {s[2], s[1], s[0], enable});
+    AND_4 and0(out[0], {not_s[2], not_s[1], not_s[0], enable});
+    AND_4 and1(out[1], {not_s[2], not_s[1], s[0], enable});
+    AND_4 and2(out[2], {not_s[2], s[1], not_s[0], enable});
+    AND_4 and3(out[3], {not_s[2], s[1], s[0], enable});
+    AND_4 and4(out[4], {s[2], not_s[1], not_s[0], enable});
+    AND_4 and5(out[5], {s[2], not_s[1], s[0], enable});
+    AND_4 and6(out[6], {s[2], s[1], not_s[0], enable});
+    AND_4 and7(out[7], {s[2], s[1], s[0], enable});
 endmodule
