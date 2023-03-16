92cbdbc55a357e236418794c3e47ff43b09ee563
dawidogg <dawidogg.feed@gmail.com>
2023-03-16 16:46:15 +0300

    Update

---
+++ decoder_tb.v
@@ -0,0 +1,11 @@
+module decoder_tb;
+    reg[2:0] combination = 3'b000;
+    wire[7:0] out;
+    Decoder_3_8 myDecoder(out, combination);
+    initial begin
+        $display("Testing decoder...");
+        $monitor("%b %b", combination, out);
+        repeat(7)
+            #10 combination = combination + 1;
+    end
+endmodule;
--- mux_decoder.v
+++ mux_decoder.v
@@ -1,22 +1,40 @@
-module MUX_8_1(y, s0, s1, s2, d0, d1, d2, d3, d4, d5, d6, d7);
-    output y;
-    input s0, s1, s2, d0, d1, d2, d3, d4, d5, d6, d7;
-    wire not_s0, not_s1, not_s2;
+module MUX_8_1(out, s, d);
+    output out;
+    input[2:0] s;
+    input[7:0] d;
+    wire[2:0] not_s;
     wire[7:0] minterm;
-    NOT n0(not_s0, s0);
-    NOT n1(not_s1, s1);
-    NOT n2(not_s2, s2);
-    AND_4 a0(minterm[0], {not_s0, not_s1, not_s2, d0});
-    AND_4 a1(minterm[1], {not_s0, not_s1, s2, d1});
-    AND_4 a2(minterm[2], {not_s0, s1, not_s2, d2});
-    AND_4 a3(minterm[3], {not_s0, s1, s2, d3});
-    AND_4 a4(minterm[4], {s0, not_s1, not_s2, d4});
-    AND_4 a5(minterm[5], {s0, not_s1, s2, d5});
-    AND_4 a6(minterm[6], {s0, s1, not_s2, d6});
-    AND_4 a7(minterm[7], {s0, s1, s2, d7});
-    OR_8 o(y, minterm);
+    
+    NOT n0(not_s[0], s[0]);
+    NOT n1(not_s[1], s[1]);
+    NOT n2(not_s[2], s[2]);
+    AND_4 a0(minterm[0], {not_s[2], not_s[1], not_s[0], d[0]});
+    AND_4 a1(minterm[1], {not_s[2], not_s[1], s[0], d[1]});
+    AND_4 a2(minterm[2], {not_s[2], s[1], not_s[0], d[2]});
+    AND_4 a3(minterm[3], {not_s[2], s[1], s[0], d[3]});
+    AND_4 a4(minterm[4], {s[2], not_s[1], not_s[0], d[4]});
+    AND_4 a5(minterm[5], {s[2], not_s[1], s[0], d[5]});
+    AND_4 a6(minterm[6], {s[2], s[1], not_s[0], d[6]});
+    AND_4 a7(minterm[7], {s[2], s[1], s[0], d[7]});
+    OR_8 o(out, minterm);
 endmodule
 
 module Decoder_3_8(out, s);
+    output[7:0] out;
+    input[2:0] s;
+    wire[2:0] not_s;
+    parameter enable = 1'b1;
 
+    NOT n0(not_s[0], s[0]);
+    NOT n1(not_s[1], s[1]);
+    NOT n2(not_s[2], s[2]);
+
+    AND_4 a0(out[0], {not_s[2], not_s[1], not_s[0], enable});
+    AND_4 a1(out[1], {not_s[2], not_s[1], s[0], enable});
+    AND_4 a2(out[2], {not_s[2], s[1], not_s[0], enable});
+    AND_4 a3(out[3], {not_s[2], s[1], s[0], enable});
+    AND_4 a4(out[4], {s[2], not_s[1], not_s[0], enable});
+    AND_4 a5(out[5], {s[2], not_s[1], s[0], enable});
+    AND_4 a6(out[6], {s[2], s[1], not_s[0], enable});
+    AND_4 a7(out[7], {s[2], s[1], s[0], enable});
 endmodule
